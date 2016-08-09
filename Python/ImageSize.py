cb7256a598cfad00fda3a4965b4c355a22141a28
JcMan <pjcmail@163.com>
2016-08-09 18:33:14 +0800

    添加ImageSize.py

---
+++ Python/ImageSize.py
@@ -0,0 +1,54 @@
+# -*-coding:utf-8 -*-
+#  python 2.7
+import os
+import sys
+import PIL.Image as Image
+
+def changeAll():
+    path = os.getcwd()
+    files = os.listdir(path)
+    result = os.path.join(path,'result')
+    if not os.path.exists(result):
+        os.mkdir(result)
+    for file in files:
+        if file.lower().endswith('png') or file.lower().endswith('jpg'):
+            filepath = os.path.join(path,file)
+            filetype = os.path.splitext(filepath)[1].replace('.','').upper()
+            if filetype == 'JPG':
+                filetype = 'JPEG'
+            img = Image.open(filepath)
+            scale = float(sys.argv[1])
+            width = int(img.size[0]*scale)
+            height = int(img.size[1]*scale)
+            img = img.resize((width,height))
+            img.save(os.path.join(result,file),filetype,quality = 99)
+            print(file+' changed!')
+
+
+def changeOne():
+    path = os.getcwd()
+    filepath = os.path.join(path,sys.argv[1])
+    result = os.path.join(path,'result')
+    if not os.path.exists(filepath):
+        print(sys.argv[1]+' not exist!')
+    else:
+        filetype = os.path.splitext(filepath)[1].replace('.','').upper()
+        if filetype == 'JPG':
+            filetype = 'JPEG'
+        result = os.path.join(path,'result')
+        if not os.path.exists(result):
+            os.mkdir(result)
+        img = Image.open(filepath)
+        scale = float(sys.argv[2])
+        width = int(img.size[0]*scale)
+        height = int(img.size[1]*scale)
+        img = img.resize((width,height))
+        img.save(os.path.join(result,sys.argv[1]),filetype,quality = 99)
+        print(sys.argv[1]+' changed!')
+
+
+if __name__ == '__main__':
+    if len(sys.argv)==2:
+        changeAll()
+    else:
+        changeOne()
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Jc_Script
-日常用到的一些脚本
+这是我日常用到的一些脚本
+- ImageSize.py
+    这个脚本的作用是改变图片的大小，有下面两种使用方式：
+    1. python ImageSize.py 0.3
+    2. python ImageSize.py a.png 0.3
+
+    第一种使用方式是将文件夹下的所有图片都变为原来的0.3，第二种使用方式则将`a.png`变为0.3
